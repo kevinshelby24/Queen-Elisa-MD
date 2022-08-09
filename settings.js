cd605f3806622195e2bb49d6b6d55317efe9c3e4
kevinshelby24 <110912708+kevinshelby24@users.noreply.github.com>
2022-08-09 20:47:35 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,14 +32,14 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.owner = ['081222193023'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['081222193023'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['081222193023'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'Chxnyy' // Your name ඔබේ නම 
+global.botnma = 'bot slebew' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = 'Chxmyy' //ownernama,ownername
+global.packname = 'Chxmyy Sticker' // Sticker package name 
+global.author = 'Chxmyy' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
@@ -57,10 +57,10 @@ global.welcome = `
 `
 // වෙල්කම් එක යවන්න ඔනි නම් add කියලා දාන්න 
 global.sendwelcome = `add`
-// Good bye එක ඔනි නම් remove කියලා දාන්න
+// Good bye selamat tinggal remove
 global.sendgoodbye = `remove`
 // Caption 
-global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
+global.cap = `© generate by CHXMYY`
 global.mess = {
     success: '✅ Done!',
     admin: 'you must be admin to use this cmd!',
